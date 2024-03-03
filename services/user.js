836a38115ef53ef1bd4ea856a9dcda903f96d665
Dean Cohen <Dean5102000@gmail.com>
2024-03-03 23:34:15 +0200

    Modified the user service and created a function to login the user and a function to generate a token to the user

--- services/user.js
+++ services/user.js
@@ -21,4 +21,32 @@ const createUser = async (username, displayName, password, profilePic) => {
 // Here I need to have a check whether the username is already taken (which means he is in the DB already, and if so I need to prompt a message) - Done upstairs
 
 }
-module.exports = { createUser }
+
+
+const loginUser = async (username, password) => {
+ // Find the user in the database based on the provided username
+ const user = await User.findOne({ username });
+
+ // If no user is found or the password doesn't match, throw an error
+ if (!user || user.password !== password) {
+   throw new Error('Incorrect username or password');
+ }
+// If the user exists in the database
+ return user;
+};
+
+// Function to generate a JWT to the user
+const generateAndSaveToken = async (user) => {
+  // Generate a JWT token
+  const token = jwt.sign({ username: user.username }, 'secret_key', { expiresIn: '1h' });
+
+  // Save the token to the database
+  // Example code assuming User model has a tokens array field
+  user.tokens.push(token);
+  await user.save();
+
+  return token;
+};
+
+module.exports = { createUser, loginUser, generateAndSaveToken }
+
