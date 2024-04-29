803977b6843e7c6a1f9bd30701b5df0e8a10a92c
Dean Cohen <Dean5102000@gmail.com>
2024-04-29 14:34:02 +0300

    Modified the function to check for url's by a global regex from the Internet.

--- utils/BloomFilterHelper.js
+++ utils/BloomFilterHelper.js
@@ -1,9 +1,11 @@
-// ** THIS FUNCTION WORKS FINE WITH THE BASIC PATTERN **
-// const checkBlacklistedURL = (postText) => {
-//   const blacklistedPattern = /LOLOBALOLO/i; // This is how we define regex.
-//   return blacklistedPattern.test(postText); //Returns true if there is a match between the pattern and the text.
-// };
+// The function to check for blacklisted URL- Returns true if there's a match and false otherwise.
+// It looks like this function works fine finding a matching blacklisted url.
+const checkBlacklistedURL = (postText) => {
+  const urlRegex = /\b(?:https?:\/\/)?(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:\/\S*)?\b/g;
+  //Define the regex for the URL
+  return urlRegex.test(postText); //Returns true if there is a match between the pattern and the text.
+};
 
-// module.exports = checkBlacklistedURL;
+module.exports = checkBlacklistedURL;
 
 
