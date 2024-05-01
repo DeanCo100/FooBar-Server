0353b2a8fb954a2bb50831e79d0db9f65b286830
Dean Cohen <Dean5102000@gmail.com>
2024-05-01 14:06:10 +0300

    Added the connection to the TCP server in the app.js file.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const net = require('net');
+
 
 var app = express();
 
@@ -25,6 +27,40 @@ mongoose.connect(process.env.CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+// Connect to the TCP server:
+// Define the TCP server address and port
+const serverAddress = process.env.TCP_IP_ADDRESS;
+console.log('hop hop');
+console.log(serverAddress)
+const serverPort = process.env.TCP_PORT;
+console.log('hop hop');
+console.log(serverPort)
+// Create a new TCP client
+const client = net.createConnection({ host: serverAddress, port: serverPort }, () => {
+  // Once connected, you can send messages to the server
+  console.log('Connected to TCP server');
+
+  // Example message: "ADD www.example.com"
+  const message = 'ADD www.example.com';
+  client.write(message); // Send message to the server
+});
+
+// Listen for data from the server
+client.on('data', (data) => {
+  console.log('Received data from server:', data.toString());
+});
+
+// Handle errors
+client.on('error', (err) => {
+  console.error('Error:', err);
+});
+
+// Handle disconnection
+client.on('end', () => {
+  console.log('Disconnected from server');
+});
+
 
 
 
