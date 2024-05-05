3b45e4fc08483f0ad04761a1242e64d60cde0220
Dean Cohen <Dean5102000@gmail.com>
2024-05-05 09:26:13 +0300

    Added 2 more urls to the blacklist and added comments.

--- utils/BloomFilterHelper.js
+++ utils/BloomFilterHelper.js
@@ -4,7 +4,7 @@ const { search } = require('../routes/user');
 
 const serverAddress = process.env.TCP_IP_ADDRESS;
 const serverPort = process.env.TCP_PORT;
-// The function to extract the urls from the postText and send them to check in the bloomfilter.
+// The function to extract the urls from the postText and send them to checkInBloom function
 const checkBlacklistedURL = async (postText) => {
   try {
     // The Regex for URL
@@ -23,14 +23,7 @@ const checkBlacklistedURL = async (postText) => {
 
       return responses.some(response => response === true);
 
-    // // Iterate over each URL and check if it's blacklisted
-    // for (const url of urls) {
-    //   const isBlacklisted = await checkInBloom(url);
-    //   console.log(isBlacklisted);
-    //   if (isBlacklisted) {
-    //     return true; // Return true if any URL is blacklisted
-    //   }
-    // }
+
     }
   } catch (error) {
     throw error;
@@ -41,6 +34,7 @@ const callSearch = async (urlToCheck) => {
   return await checkInBloom(urlToCheck);
 }
 
+// The function the receives the URLs and send them to the BF
 const checkInBloom = async (url) => {
   try {
     return new Promise((resolve, reject) => {
@@ -49,7 +43,7 @@ const checkInBloom = async (url) => {
         console.log('Connected successfully to the TCP server');
         client.write(`2 ${url.trim()}`); 
       });
-
+      // Handle data received
       client.on('data', (data) => {
         const dataResponse = data.toString().trim();
         console.log('received data from TCP: ', dataResponse);
@@ -75,96 +69,6 @@ const checkInBloom = async (url) => {
     throw error;
     }
 };
-
-
-
-    // // Create a new TCP client connection
-    // const client = net.createConnection({ host: process.env.TCP_IP_ADDRESS, port: process.env.TCP_PORT });
-
-    // // Define a promise to handle TCP events
-    // const checkPromise = new Promise((resolve, reject) => {
-    //   let responseData = ''; // Accumulate response data from the server
-
-    //   client.on('connect', () => {
-    //     // Send the URL to the TCP server for checking
-    //     client.write(`2 ${url}\n`); // Use "2" for checking in the Bloom filter
-    //   });
-
-    //   // Listen for data from the server
-    //   client.on('data', (data) => {
-    //     responseData += data.toString(); // Accumulate response data
-    //     console.log(responseData);
-
-    //   });
-
-    //     // Process the received data and resolve the promise
-    //     const isBlacklisted = responseData.trim() === 'true';
-    //     resolve(isBlacklisted);
-
-      // // Handle errors
-      // client.on('error', (err) => {
-      //   reject(err);
-      // });
-
-      // // Handle disconnection
-      // client.on('end', () => {
-      //   console.log('Disconnected from server');
-      //   client.destroy();
-      //   // // Process the received data and resolve the promise
-      //   // const isBlacklisted = responseData.trim() === 'true';
-      //   // resolve(isBlacklisted);
-      // });
-    // });
-    // // Resolve the promise after receiving all data and processing it
-    // // checkPromise.then(() => {
-    // //   // Process the received data and resolve the promise
-    // //   const isBlacklisted = responseData.trim() === 'true';
-    // //   resolve(isBlacklisted);
-    // // });
-
-    // return checkPromise;
-//     } catch (error) {
-//     throw error;
-//     }
-// };
-
-
-// const checkInBloom = async (url) => {
-//   try {
-//     // Create a new TCP client connection
-//     const client = net.createConnection({ host: process.env.TCP_IP_ADDRESS, port: process.env.TCP_PORT });
-
-//     // Define a promise to handle TCP events
-//     const checkPromise = new Promise((resolve, reject) => {
-//       client.on('connect', () => {
-//         // Send the URL to the TCP server for checking
-//         client.write(`2 ${url}\n`); // Use "2" for checking in the Bloom filter
-
-//         // Listen for data from the server
-//         client.once('data', (data) => {
-//           const response = data.toString().trim();
-//           const isBlacklisted = response === 'true';
-//           resolve(isBlacklisted);
-//         });
-
-//         // Handle errors
-//         client.on('error', (err) => {
-//           reject(err);
-//         });
-
-//         // Handle disconnection
-//         client.on('end', () => {
-//           console.log('Disconnected from server');
-//           client.destroy();
-//         });
-//       });
-//     });
-
-//     return checkPromise;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
 
 
 module.exports = {checkBlacklistedURL, checkInBloom};
