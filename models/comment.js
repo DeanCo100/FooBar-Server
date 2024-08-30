4e0c07d061abef45c011cd51098cb3e2054d8f81
Dean Cohen <Dean5102000@gmail.com>
2024-08-30 11:20:47 +0300

    Created the new Comment Schema

---
+++ models/comment.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Each comment should have The USERNAME, the USERPIC and the TEXT.
+// NOTE: Each user now should have a list of his comments (and only them he can delete).
+// NOTE: Each Post should have a list of his comments.
+
+const CommentSchema = new Schema ({
+  username: {
+    type: String,
+    required: true
+  },
+  displayName: {
+    type: String,
+    required: true
+  },
+  profilePic: {
+    type: String, 
+    required: true
+  },
+  commentText: {
+    type: String,
+    required: true
+  }
+})
+
+const User = mongoose.model('Comment', CommentSchema);
+
+module.exports = Comment;
